36a96689b6282be02c1cf7caa6e5c9f62f2c1720
Sreerag Mohan <sreeraagmohancv@gmail.com>
2024-06-22 14:45:16 +0530

    admin page added

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+import { AuthOptions } from '@stacks/connect'
+
+export const appDetails: AuthOptions['appDetails'] = {
+  name: 'Beanstalk Exchange',
+  icon: 'https://freesvg.org/img/youk-k-Beanstalk.png',
+}
+
+export const contractOwnerAddress = 'ST2RC7FMFZWK24RERQ40SD6H2TE6HZ6WT2G4ZE4A3';
